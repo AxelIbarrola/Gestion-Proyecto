943081a7ed35808a396fbefff7c35121355db1bb
Axel Ibarrola <axelibarro@gmail.com>
2025-03-30 19:32:58 -0300

    agregada la clase de configuracion para el costo fijo de las tareas, solo se puede generar una instancia de la misma (la cual no se puede modificar) y se puede cambiar el costo fijo con la funcion cambiarCosto()

---
+++ configuracion_costos.js
@@ -0,0 +1,24 @@
+class Configuracion {
+    constructor() {
+        if (!Configuracion.instancia) {
+            this.costoFijo = 10;
+            Configuracion.instancia = this
+        }
+        return Configuracion.instancia
+    }
+
+    cambiarCosto(nuevoCosto) {
+        this.costoFijo = nuevoCosto
+    }
+
+    obtenerCostoFijo() {
+        return this.costoFijo
+    }
+
+}
+
+const instanciaConfiguracion = new Configuracion()
+
+Object.freeze(instanciaConfiguracion)
+
+module.exports = instanciaConfiguracion;
